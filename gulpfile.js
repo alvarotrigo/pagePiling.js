5235aa5f0c05d12c702411b5a33d0ef871fb8e06
Álvaro <atrigol@gmail.com>
2016-07-07 16:31:40 +0200

    - Added gulp and `dist` files - Added package.json file - Added gitignore - Improved credits in CSS and JS files

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-clean-css');
+
+gulp.task('css', function() {
+    gulp.src('./jquery.pagepiling.css')
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('./dist'))
+        .pipe(minifyCss({
+            compatibility: 'ie8',
+            advanced: false,
+            keepSpecialComments: '1'
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.')) 
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', function() {
+    gulp.src('./jquery.pagepiling.js')
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('./dist'))
+        .pipe(uglify({
+            preserveComments: 'license'
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['css', 'js']);
